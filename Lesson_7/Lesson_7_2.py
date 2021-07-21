2019cdfcf22a3c89e4a8d8f59359811a21b1778b
Алексеенко Виталий <alekseenko.v@morepiva.ukr>
2021-07-21 17:05:48 +0300

    переименовал урок 7 на 8

--- Lesson_7/Lesson_7_2.py
+++
@@ -1,6 +0,0 @@
-myList = [1, 1]
-steps = int(input('Введите количество ступеней - '))
-for i in range(steps - 2):
-    myList.append(myList[len(myList) - 1] + myList[len(myList) - 2])
-print("Количество способов чтобы забраться на лестницу - " + str(myList[len(myList)-1]))
-
