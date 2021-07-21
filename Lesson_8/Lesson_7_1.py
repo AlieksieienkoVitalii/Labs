15bf804fd7e653f0e58871a8188bb7c4a869f5ff
Алексеенко Виталий <alekseenko.v@morepiva.ukr>
2021-07-21 17:08:45 +0300

    Изменение названия папки с урок 7 на урок 8

---
+++ Lesson_8/Lesson_7_1.py
@@ -0,0 +1,7 @@
+myList = [1, 4, 7, 54, 8, 9, 234, 78, 35]
+print("Значения моего списка - " + str(myList))
+print("Максимальное число в моем списке - " + str(max(myList)))
+print("Минимальное число в моем списке - " + str(min(myList)))
+print("Сумма чисел в моем списке - " + str(sum(myList)))
+print("Среднее значение моего списка - " + str(sum(myList) / len(myList)))
+
